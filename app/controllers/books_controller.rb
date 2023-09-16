ef12a7648841f4200cabe464149f609bf549c23f
GotouAsahi <gotou.asahi.infratop@gmail.com>
2023-09-16 21:26:49 +0900

    a_OK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,17 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user, only: %i[edit update destroy]
 
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
     @book_comment = BookComment.new
   end
 
   def index
-    @books = Book.all
+    @books = Book.sort_favorites
     @book = Book.new
   end
 
@@ -16,21 +19,20 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book), notice: 'You have created book successfully.'
     else
       @books = Book.all
       render 'index'
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book), notice: 'You have updated book successfully.'
     else
-      render "edit"
+      render 'edit'
     end
   end
 
@@ -47,8 +49,8 @@ class BooksController < ApplicationController
 
   def ensure_correct_user
     @book = Book.find(params[:id])
-    unless @book.user == current_user
-      redirect_to books_path
-    end
+    return if @book.user == current_user
+
+    redirect_to books_path
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,10 +3,16 @@ class Book < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :view_counts, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
+  end
+
+  def self.sort_favorites
+    includes(:favorites).sort_by { |x| x.favorites.where(created_at: 1.week.ago..Time.now).count }.reverse
   end
 end
