7b746e297b8d86428170173db43f86142b239a37
GotouAsahi <gotou.asahi.infratop@gmail.com>
2023-09-20 22:03:54 +0900

    c_totyuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root :to =>"homes#top"
-  get "home/about"=>"homes#about"
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resources :book_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
+  resources :books, only: %i[index show edit create destroy update] do
+    resources :book_comments, only: %i[create destroy]
+    resource :favorites, only: %i[create destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update] do
-    resource :relationships, only: [:create, :destroy]
-  	get 'followings' => 'relationships#followings', as: 'followings'
-  	get 'followers' => 'relationships#followers', as: 'followers'
+  resources :users, only: %i[index show edit update] do
+    resource :relationships, only: %i[create destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  resources :groups, except: [:destroy] do
+    get 'join' => 'groups#join'
+    delete 'exit' => 'groups#exit'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/search', to: 'searches#search'
 end
